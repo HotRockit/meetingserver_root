aa8a4172b703a1dc26a4b1b41372e951119d49a0
HotRockit <wdy1607020322@gmail.com>
2021-09-12 22:17:25 +0800

    修复当会议室名称为空时可以更新以及查找指定会议室的会议没有值的错误

--- dao/dao.go
+++ dao/dao.go
@@ -42,7 +42,7 @@ func (appDao *AppDao) QueryMeetingDao(currentPage int, num int, name string, use
 		} else if name == "no" && username != "no" && time == "no" {
 			appDao.Where("user = ?", username).Find(&meetings)
 		} else if name != "no" && username == "no" && time == "no" {
-			appDao.Where("borrow_time = ?", time).Find(&meetings)
+			appDao.Where("meeting_room_name = ?", time).Find(&meetings)
 		} else if name == "no" && username == "no" && time == "no" {
 			appDao.Find(&meetings)
 		} else {
